1a621c2db9ebb57d2b488ce9aada6f39b0e75575
drone <sa2rn.set@gmail.com>
2023-08-29 12:29:45 +0000

    Updates

---
+++ webapp/dist/precache-manifest.6369444c9493dfd082130d9a3270de52.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2863f1e1db0bdc1de964",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/10.896d9542400fe61797ad.js"
+  },
+  {
+    "revision": "c7aa15279b837caabbca",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/11.b77996d63160adada322.css"
+  },
+  {
+    "revision": "c7aa15279b837caabbca",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/11.d2f846648e402beef518.js"
+  },
+  {
+    "revision": "3da81d863fd331652338",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/12.16cced3de4c88b1a8b28.css"
+  },
+  {
+    "revision": "3da81d863fd331652338",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/12.74242a21488f4cd7e811.js"
+  },
+  {
+    "revision": "7c92b06e01738353dc99",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/13.04875263fe481776e277.css"
+  },
+  {
+    "revision": "7c92b06e01738353dc99",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/13.11f9831ee0f117c30cb1.js"
+  },
+  {
+    "revision": "dd69d5b36310d0b1e918",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/14.5ca2f257e6ab7cbe1f2b.js"
+  },
+  {
+    "revision": "66ee01db0eea4916e707",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/15.ee0b8551dfa4bfbb9a18.js"
+  },
+  {
+    "revision": "f452a2df2bf6add6d219",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/16.f42f0199d6a2657f8d0c.js"
+  },
+  {
+    "revision": "2dac8dd14d0aa42c6e3a",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/3.ba04ddfdeeb7bb1226fe.js"
+  },
+  {
+    "revision": "3254f10c200e1b4f65a3",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/6.70a0a9d1fc629bdd900a.js"
+  },
+  {
+    "revision": "3254f10c200e1b4f65a3",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/6.ca62b17efc1f0d97c1ec.css"
+  },
+  {
+    "revision": "60f60c76a288cdce8d54",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/7.00516248f2ebfbab3659.css"
+  },
+  {
+    "revision": "60f60c76a288cdce8d54",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/7.88a00e3bf7b061431b86.js"
+  },
+  {
+    "revision": "28cbaab63a936b16ea5a",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/8.097057c1c3054b32f3c8.js"
+  },
+  {
+    "revision": "1b5f54ef29d139dd6c88",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/9.ad460e3df21ec33b1e86.css"
+  },
+  {
+    "revision": "1b5f54ef29d139dd6c88",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/9.d00d516e39a200951d2a.js"
+  },
+  {
+    "revision": "f8ab3e4f8698da1a0c1f",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/auth.036e3d89db0420312c89.js"
+  },
+  {
+    "revision": "9cdbd17440c9effcc246b46a1eb0ef1f",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/landings/img/editorGuide/1.webp"
+  },
+  {
+    "revision": "1fd850dccc449f73b94e2c761c026be9",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/landings/img/editorGuide/2.webp"
+  },
+  {
+    "revision": "95f49cb72d919467be4b6bd922ece338",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/landings/img/editorGuide/3.webp"
+  },
+  {
+    "revision": "2c441b069b581822d8686626f6383dbf",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/landings/img/instagramTip.jpg"
+  },
+  {
+    "revision": "e4fe68503ab5d004deb31e43636a0a7c",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/landings/img/poweredByGiphy.png"
+  },
+  {
+    "revision": "a2b5ad23d832892f476af3fbfdbd1554",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/landings/img/tiktokTip.jpg"
+  },
+  {
+    "revision": "293db563b88ddf7283de",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/locales-en.dd91f40cb5800f8b18b3.js"
+  },
+  {
+    "revision": "e911009e3fff1a5fd6cf",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/locales-ru.0e3ee72660e69bce2404.js"
+  },
+  {
+    "revision": "60acd245a1ff87a70cbc",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/locales-uk.f33414c322404d5d265d.js"
+  },
+  {
+    "revision": "0807abdbb4ec558f2285640932f3e0b9",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/colors.png"
+  },
+  {
+    "revision": "f3bd65ea1196213bf68c1f5dd21576d3",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/copylink_img.svg"
+  },
+  {
+    "revision": "c64b8c38b47ef2273aaa94b5416bcafc",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/icon-go-back.svg"
+  },
+  {
+    "revision": "72de1dc8f541c9269eec96c4f46b1aa8",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/logo_branding.svg"
+  },
+  {
+    "revision": "3fa0c31a15e89e29a3ebaba55b24c379",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/payments/ae_logo.png"
+  },
+  {
+    "revision": "71893604a3ae2f105a7f0fab37a1d55a",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/payments/ae_logo.svg"
+  },
+  {
+    "revision": "a531c12f9a7ba5d86f9d6fb44d795d2d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/payments/mc_logo.png"
+  },
+  {
+    "revision": "2b1c8b892eebac23041abf02d46a6577",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/payments/powered_by_stripe_ol.svg"
+  },
+  {
+    "revision": "20ba93da6775a58b22fdcfa6b228cdcf",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/payments/stripe.png"
+  },
+  {
+    "revision": "dc2d0d3e56360904bc6fe0223922cf88",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/payments/visa_light.svg"
+  },
+  {
+    "revision": "da6e425698d430ea527cde3ce2b77f9c",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/payments/visa_logo.png"
+  },
+  {
+    "revision": "817378fe8865e223a2344c089d605d33",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/ui/img/select_arrow.svg"
+  },
+  {
+    "revision": "b54bbf3a4aac22fe0232",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/webapp.47d7c74550bc16d8e37b.css"
+  },
+  {
+    "revision": "b54bbf3a4aac22fe0232",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.15.3-alpha.5/webapp/dist/webapp.762d9501bd8b27ca2aa0.js"
+  }
+]);
