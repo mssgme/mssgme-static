df16934ffb90c703a2881a71f68a73ac701d891d
drone <ankhzet@noreply.localhost>
2024-02-02 12:20:36 +0000

    Updates

---
+++ webapp-legacy/dist/precache-manifest.0e500a9ead57109fe65e72b83ccb1d7b.js
@@ -0,0 +1,422 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45dbd964f15d36a349c2",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/4.b1f19f3c21f7aac7223e.js"
+  },
+  {
+    "revision": "6de0de441b93065f997d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/5.1d12164cb70187d5ad6e.js"
+  },
+  {
+    "revision": "f3bd65ea1196213bf68c1f5dd21576d3",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/copylink_img.svg"
+  },
+  {
+    "revision": "fc2eb376773b7e5ff6ebcb290eed3283",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/email.svg"
+  },
+  {
+    "revision": "7e37122a5b7513c6c9fea2ba65573444",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/embed.svg"
+  },
+  {
+    "revision": "dcd55866f6937a8ffa0296c7f5af5480",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/link.svg"
+  },
+  {
+    "revision": "119824e68a157e3e2f15525d856287aa",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/logo-white.svg"
+  },
+  {
+    "revision": "6012aaad61a914b5338a19301f32920c",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/messengers/messenger-rounded.svg"
+  },
+  {
+    "revision": "17977f4803147bd5f36d26ac2c0aaae1",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/messengers/skype-rounded.svg"
+  },
+  {
+    "revision": "ee647eae215c412b1225c2c9de973599",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/messengers/telegram-rounded.svg"
+  },
+  {
+    "revision": "c884d0910f24c484020d23c12a15e628",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/messengers/viber-rounded.svg"
+  },
+  {
+    "revision": "21fc6695ed6c726d118966aa5b6654e8",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/messengers/vk-rounded.svg"
+  },
+  {
+    "revision": "54c1181c0f76de51421e983c305a14cf",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/messengers/whatsapp-rounded.svg"
+  },
+  {
+    "revision": "5e8dfa0d89dadd28b9c53dd54cf92411",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/cards/img/qr.svg"
+  },
+  {
+    "revision": "91eccac778aba6cb0deb",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/chatbot.3b0f3f83d91062614e20.css"
+  },
+  {
+    "revision": "91eccac778aba6cb0deb",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/chatbot.cd7c76628ac0ee20dd3d.js"
+  },
+  {
+    "revision": "1bc7873fb084cebf4d3af61c4b4e8ebc",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/Avatar.svg"
+  },
+  {
+    "revision": "1b353d8b6b5c097665bbbe4915c7ee09",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/aboutProBg.jpg"
+  },
+  {
+    "revision": "942bc13c91138d1994981a0fbe27c954",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/aboutProLogo.svg"
+  },
+  {
+    "revision": "d8e61a8798bd7e0656a434673d447406",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/aboutProPhone.png"
+  },
+  {
+    "revision": "3fa0c31a15e89e29a3ebaba55b24c379",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/ae_logo.png"
+  },
+  {
+    "revision": "71893604a3ae2f105a7f0fab37a1d55a",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/ae_logo.svg"
+  },
+  {
+    "revision": "a19f2ceee77317c356adefffe771f263",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/blank.png"
+  },
+  {
+    "revision": "4c83d99bd2441057f20f053e46092871",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/card/logo.svg"
+  },
+  {
+    "revision": "e277c4f7dfe2c0a9eda9273687085d91",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/choose.svg"
+  },
+  {
+    "revision": "b87435261b9579c93f52de7841c5eb15",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/do.png"
+  },
+  {
+    "revision": "825212d2915bff9e9c8edaea2230ca23",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/facebook.png"
+  },
+  {
+    "revision": "49c7d19d64adab7f34a8f5b4a4531fd7",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/facebook2.svg"
+  },
+  {
+    "revision": "53647df9cd3552b24b58c7a7ff0ffa27",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/hand.png"
+  },
+  {
+    "revision": "f1887fd6d28d8d93d5cc94f9bfb53de4",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/ic_check_circle_black_24px.svg"
+  },
+  {
+    "revision": "c64b8c38b47ef2273aaa94b5416bcafc",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/icon-go-back.svg"
+  },
+  {
+    "revision": "ef829010108d0512f7a936df8850712f",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/iconWhatsapp.svg"
+  },
+  {
+    "revision": "64dd1decda4a0e9ca34ce805174db084",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/icon_arrow_grey.svg"
+  },
+  {
+    "revision": "c93475375089fc7c05484bd9f6ef567c",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/icon_share_one.png"
+  },
+  {
+    "revision": "244c68a339e316ae07c237d1c93a6a9e",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/icon_share_three.png"
+  },
+  {
+    "revision": "18f451dfd62a40cb17d6561aabe72ed4",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/icon_share_two.png"
+  },
+  {
+    "revision": "b7d5b392362c668b47fc8bbc45eb0643",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/icon_we_use_mssgme_en.svg"
+  },
+  {
+    "revision": "17109705e48bf3142abee47af574db5d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/icon_we_use_mssgme_ru.svg"
+  },
+  {
+    "revision": "54c04bdffcd8e741d9a9632801e6811e",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/insta.svg"
+  },
+  {
+    "revision": "e6fbb86a0335350fc06c668205dd75b2",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/insta_share.png"
+  },
+  {
+    "revision": "6417224ce3f4cfe96325604cbddb914d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/logo.svg"
+  },
+  {
+    "revision": "7d08068c7e40550142e19761acfea06d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/logo_bot.svg"
+  },
+  {
+    "revision": "25620cddeec15185c2640f26532553fe",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/man.png"
+  },
+  {
+    "revision": "a531c12f9a7ba5d86f9d6fb44d795d2d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/mc_logo.png"
+  },
+  {
+    "revision": "1398d2bab247c05d6832aad653609429",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-facebook-login.svg"
+  },
+  {
+    "revision": "38604db64526c1dca19198d18fce2eb4",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-facebook.svg"
+  },
+  {
+    "revision": "8359a14374f7e66f5ec46aaa6ef85870",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-line.svg"
+  },
+  {
+    "revision": "c6c3c303746870daa5d4e541c4982f6d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-skype.svg"
+  },
+  {
+    "revision": "cf671f79d64026afad4ca61ea65225dc",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-telegram.svg"
+  },
+  {
+    "revision": "be0d791061a548da5dc92a069bce5be7",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-viber.svg"
+  },
+  {
+    "revision": "bbef82dcb15ed2a23472be660ba01328",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-vk.svg"
+  },
+  {
+    "revision": "03359ace09ffa6fb6e04f60d4068ee7a",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/messengers/icon-whatsapp.svg"
+  },
+  {
+    "revision": "3113ce56ceaa7467450680ad5d8d7093",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/analytics.svg"
+  },
+  {
+    "revision": "43adc8fb58b4117cd2c7682a7d556d84",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/analytics_integrations.svg"
+  },
+  {
+    "revision": "b48745a54b2c7262c9a6127c8a4b8a70",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/categories.svg"
+  },
+  {
+    "revision": "fe3ed314436c5fb5df343d8265c51283",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/cover.svg"
+  },
+  {
+    "revision": "76bfdef6617612827cb7affd0f670fa4",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/domain.svg"
+  },
+  {
+    "revision": "704c8681234ae7e5c2a6cf95b9892cd7",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/googlepay.png"
+  },
+  {
+    "revision": "2efc8ea83efc182cf6f24a90af443956",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/link.svg"
+  },
+  {
+    "revision": "20ee29d8c20238222c07abfdfecceaaa",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/messengers.svg"
+  },
+  {
+    "revision": "cd372736402f616c00cfb7df96b63b61",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/no_branding.svg"
+  },
+  {
+    "revision": "5f9676262789feb6d7eceb472c4cdadd",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/pixel.svg"
+  },
+  {
+    "revision": "0a65f742c2c3904f42bc36be11024bac",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/onboarding/support.svg"
+  },
+  {
+    "revision": "0e57eb565d01cbe55849add01080fbbd",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/power.png"
+  },
+  {
+    "revision": "2b1c8b892eebac23041abf02d46a6577",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/powered_by_stripe_ol.svg"
+  },
+  {
+    "revision": "00e11d69e821e21dc3a51b38e9bde167",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/robot.png"
+  },
+  {
+    "revision": "20ba93da6775a58b22fdcfa6b228cdcf",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/stripe.png"
+  },
+  {
+    "revision": "c6cd087ad696e04430c838e49ed4ba66",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/telegram.png"
+  },
+  {
+    "revision": "49aafe51fa7aacf7edcd760e7913afa6",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/telegram2.svg"
+  },
+  {
+    "revision": "099303a85c8d5a27c7ecbe7bd8a94ff5",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/viber.svg"
+  },
+  {
+    "revision": "9a9271b1b2b1c3f5702e490d203f2f90",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/viber2.svg"
+  },
+  {
+    "revision": "dc2d0d3e56360904bc6fe0223922cf88",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/visa_light.svg"
+  },
+  {
+    "revision": "da6e425698d430ea527cde3ce2b77f9c",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/visa_logo.png"
+  },
+  {
+    "revision": "89b6559b1d174ca4f710df247ba273d1",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/vk.svg"
+  },
+  {
+    "revision": "c1845f0684d9c90f6cc28f533c25dc72",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/whatsapp.svg"
+  },
+  {
+    "revision": "ff7d169ee393d202f04ff60334ade3a4",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/img/woman.png"
+  },
+  {
+    "revision": "92c4ad0a1fd34091e0c9db8c23245b8b",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/inbox/img/aquaman.png"
+  },
+  {
+    "revision": "def96b3bad85090c68ee",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/locales-en.f67a43fbb97a13ef69b0.js"
+  },
+  {
+    "revision": "d767cadfde844b863716",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/locales-ru.a79863e07182da2c75e9.js"
+  },
+  {
+    "revision": "e86abb17845438997fc9",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/locales-uk.565505bdedc650d47a0f.js"
+  },
+  {
+    "revision": "e38233b769e0c5e9b579",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/redirect.a8635e4fa9e39c44b116.js"
+  },
+  {
+    "revision": "e38233b769e0c5e9b579",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/redirect.d432d350113980f185ef.css"
+  },
+  {
+    "revision": "8c08f24a9167bb6b51cf",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/runtime.6d410b964db04d080756.js"
+  },
+  {
+    "revision": "f29e5bf8718fa447f487b836bcd2cfef",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/brands/facebook-messenger.svg"
+  },
+  {
+    "revision": "aeaff85db1abc6e6a041e50408270004",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/brands/skype.svg"
+  },
+  {
+    "revision": "c88b26d559165ea055fa054d7846d16b",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/brands/telegram.svg"
+  },
+  {
+    "revision": "06331d25174544de216cc672dc8a1118",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/brands/viber.svg"
+  },
+  {
+    "revision": "9e8d7d849db453813db8ed46e43332dc",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/brands/vkontakte.svg"
+  },
+  {
+    "revision": "275582bb327121a5e174f8ab87d97936",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/brands/whatsapp.svg"
+  },
+  {
+    "revision": "0807abdbb4ec558f2285640932f3e0b9",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/colors.png"
+  },
+  {
+    "revision": "f3bd65ea1196213bf68c1f5dd21576d3",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/copylink_img.svg"
+  },
+  {
+    "revision": "c64b8c38b47ef2273aaa94b5416bcafc",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/icon-go-back.svg"
+  },
+  {
+    "revision": "72de1dc8f541c9269eec96c4f46b1aa8",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/logo_branding.svg"
+  },
+  {
+    "revision": "3fa0c31a15e89e29a3ebaba55b24c379",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/payments/ae_logo.png"
+  },
+  {
+    "revision": "71893604a3ae2f105a7f0fab37a1d55a",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/payments/ae_logo.svg"
+  },
+  {
+    "revision": "a531c12f9a7ba5d86f9d6fb44d795d2d",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/payments/mc_logo.png"
+  },
+  {
+    "revision": "2b1c8b892eebac23041abf02d46a6577",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/payments/powered_by_stripe_ol.svg"
+  },
+  {
+    "revision": "20ba93da6775a58b22fdcfa6b228cdcf",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/payments/stripe.png"
+  },
+  {
+    "revision": "dc2d0d3e56360904bc6fe0223922cf88",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/payments/visa_light.svg"
+  },
+  {
+    "revision": "da6e425698d430ea527cde3ce2b77f9c",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/payments/visa_logo.png"
+  },
+  {
+    "revision": "817378fe8865e223a2344c089d605d33",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/ui/img/select_arrow.svg"
+  },
+  {
+    "revision": "28ffdee68e2318ede148",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/userpage.20f0592a912715c1efed.css"
+  },
+  {
+    "revision": "28ffdee68e2318ede148",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/userpage.6405367765fe9581e8c4.js"
+  },
+  {
+    "revision": "4f043a76c87c237678d7",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/webapp.37b5e3741146cda33961.js"
+  },
+  {
+    "revision": "4f043a76c87c237678d7",
+    "url": "https://cdn.jsdelivr.net/gh/mssgme/mssgme-static@v5.20.0-alpha.2/webapp-legacy/dist/webapp.5bc78b83e08891af74b7.css"
+  }
+]);
